e1ffe1cfd354a0c516f68be4c9aa0a66de83d03d
Andy-hs79 <andy_hs@mail.ru>
2024-04-28 16:35:44 +0300

    add email field instead of name

--- education_platform/users/models.py
+++ education_platform/users/models.py
@@ -1,12 +1,30 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+
+from .managers import CustomUserManager
 
 
-class User(models.Model):
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_("email address"), unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+
+class User(CustomUser):
     # id =
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
-    e_mail = models.CharField(max_length=50)
-    password = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=20, verbose_name="Имя")
+    last_name = models.CharField(max_length=20, verbose_name="Фамилия")
+    # e_mail = models.CharField(max_length=50)
+    # password = models.CharField(max_length=20)
 
 
 # Create your models here.
