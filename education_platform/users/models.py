1cb6f4bfd7bcd3e9aed544eaf3a4675bb7b47f39
Andy-hs79 <andy_hs@mail.ru>
2024-04-29 13:42:15 +0300

    configure user

--- education_platform/users/models.py
+++ education_platform/users/models.py
@@ -6,7 +6,7 @@ from django.utils.translation import gettext_lazy as _
 from .managers import CustomUserManager
 
 
-class CustomUser(AbstractBaseUser, PermissionsMixin):
+class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(_("email address"), unique=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
@@ -14,17 +14,11 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
     objects = CustomUserManager()
+    first_name = models.CharField(max_length=20, verbose_name="Имя")
+    last_name = models.CharField(max_length=20, verbose_name="Фамилия")
 
     def __str__(self):
         return self.email
-
-
-class User(CustomUser):
-    # id =
-    first_name = models.CharField(max_length=20, verbose_name="Имя")
-    last_name = models.CharField(max_length=20, verbose_name="Фамилия")
-    # e_mail = models.CharField(max_length=50)
-    # password = models.CharField(max_length=20)
 
 
 # Create your models here.
