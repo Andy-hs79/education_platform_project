eeb3a45381ee6d2df551f18b9d9562c9545cd625
Andy-hs79 <andy_hs@mail.ru>
2024-05-08 22:00:15 +0300

    moving models to ed_process

--- education_platform/education_process/models.py
+++ education_platform/education_process/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
+from education_platform.users.models import Student, Teacher
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Название раздела")
+    index_number = models.IntegerField()
+    description = models.TextField()
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Название группы")
+    group_teacher = models.ManyToManyField(Teacher, on_delete=models.CASCADE)
+    id_student = models.ForeignKey(Student, on_delete=models.CASCADE)
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Название курса")
+    id_creator = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    id_topic = models.ForeignKey(
+        Topic, on_delete=models.CASCADE
+    )  # в курсе несколько разделов
+    id_group = models.ForeignKey(
+        Group, on_delete=models.CASCADE
+    )  # один курс могут проходить несколько групп
+
+
+class Answer_options(models.Model):
+    answer = models.TextField()
+    true_answer = models.BooleanField()  # флаг верного ответа
+
+
+class Questions(models.Model):
+    index_number = models.IntegerField()
+    id_answer = models.ForeignKey(Answer_options, on_delete=models.CASCADE)
+
+
 # Create your models here.
--- education_platform/users/models.py
+++ education_platform/users/models.py
@@ -30,32 +30,4 @@ class Teacher(models.Model):
     id_user = models.OneToOneField(User, on_delete=models.CASCADE)
 
 
-class Topic(models.Model):
-    name = models.CharField(max_length=100, verbose_name="Название раздела")
-    index_number = models.IntegerField()
-    description = models.TextField()
-
-
-class Course(models.Model):
-    name = models.CharField(max_length=100, verbose_name="Название курса")
-    creator = models.ForeignKey(Teacher, on_delete=models.CASCADE)
-
-
-class Group(models.Model):
-    name = models.CharField(max_length=100, verbose_name="Название группы")
-    id_course = models.OneToOneField(Course, on_delete=models.CASCADE)
-    group_teacher = models.ManyToManyField(Teacher, on_delete=models.CASCADE)
-    id_student = models.ForeignKey(Student, on_delete=models.CASCADE)
-
-
-class Answer_options(models.Model):
-    answer = models.TextField()
-    true_answer = models.BooleanField()  # флаг верного ответа
-
-
-class Questions(models.Model):
-    index_number = models.IntegerField()
-    id_answer = models.ForeignKey(Answer_options, on_delete=models.CASCADE)
-
-
 # Create your models here.
